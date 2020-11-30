b5df6f0370981f8361bed1e1806f2b7979e6de7f
manlan <2733284198@qq.com>
2020-11-30 16:12:44 +0800

    2020年11月30日16:04:10 1 mysql提交代码

--- gorm/mysql.go
+++ gorm/mysql.go
@@ -6,7 +6,7 @@ import (
 )
 
 type User struct {
-	ID   int64
+	ID   int64 `gorm:"AUTO_INCREMENT"` // 设置 num 为自增类型
 	Name string
 	Age  int64
 }
@@ -15,10 +15,15 @@ func main() {
 	db, _ := gorm.Open("mysql", "root:root@/go?charset=utf8&parseTime=True&loc=Local")
 
 	user := User{Name: "q1mi", Age: 18}
+	db.Create(&user) // 创建user
 
-	db.NewRecord(user) // 主键为空返回`true`
-	db.Create(&user)   // 创建user
-	db.NewRecord(user) // 创建`user`后返回`false`
+	//db.NewRecord(user) // 主键为空返回`true`
+
+	user2 := User{Name: "q1mi2", Age: 18}
+	//db.NewRecord(user2) // 主键为空返回`true`
+	db.Create(&user2) // 创建user
+
+	//db.NewRecord(user) // 创建`user`后返回`false`
 
 	defer db.Close()
 }
